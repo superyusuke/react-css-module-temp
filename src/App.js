89a93ea49e576f34876b04493cf6a0ba6ce4a937
NAKANISHI Yusuke <nakanishiyusuke0000@gmail.com>
2017-10-19 09:26:46 +0900

    最初のコミット

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        <p className={styles.blue}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, harum!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
